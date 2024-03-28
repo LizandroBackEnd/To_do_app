da047988d94c21f770edb313c3b22618a95190a8
LizandroBackEnd <codeversebacked@gmail.com>
2024-03-28 11:11:26 -0600

    ci: Structured the layout the aplication and lack add function the button Hide completed | Dev: Lizandro

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,25 @@
 import * as React from 'react'; 
-import { StyleSheet, Text, View, Image} from 'react-native'; 
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'; 
 import TodoList from '../components/TodoList'; 
 import { todosData } from '../data/todos';
 
-export default function Home() {
+export default function Home() { 
+    const [localData, setLocalData] = React.useState(  
+        todosData.sort((a, b) => {return a.isCompleted - b.isCompleted})
+    );
   return ( 
     <View style={styles.container}>  
         <Image  
             source={{ uri: 'https://i.pinimg.com/564x/a9/b2/fd/a9b2fdb12dcf8a29b82b1ba291bcefac.jpg' }} 
             style={styles.pic} 
         />    
-        <Text style={styles.title}>Today</Text>  
-        <TodoList todosData={todosData.filter(todo => todo.isToday)} />
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}> 
+            <Text style={styles.title}>Today</Text>  
+            <TouchableOpacity>  
+            <Text style={{ color: '#3478f6' }}>Hide Completed</Text>     
+            </TouchableOpacity> 
+        </View>
+        <TodoList todosData={localData.filter(todo => todo.isToday)} />
         <Text style={styles.title}>Tomorrow</Text>  
         <TodoList todosData={todosData.filter(todo => !todo.isToday)} />
         <TodoList />
