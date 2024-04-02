bec88e5c7d6f3392836eb82c57d0629dbf1126ae
LizandroBackEnd <codeversebacked@gmail.com>
2024-04-02 10:59:18 -0600

    ci: Add function deleted the task | Dev: Lizandro

--- components/Todo.js
+++ components/Todo.js
@@ -1,7 +1,12 @@
 import * as React from 'react'; 
-import { View, Text, StyleSheet } from 'react-native';  
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';  
 import Checkbox from './Checkbox'; 
-import moment from 'moment';
+import moment from 'moment'; 
+import { MaterialIcons } from '@expo/vector-icons';
+import { deleteTodoReducer } from '../redux/todosSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { useDispatch, useSelector } from 'react-redux'; 
+
   
 export default function Todo({ 
     id, 
@@ -11,22 +16,46 @@ export default function Todo({
     hour
 }){  
      
-    const [localHour, setLocalHour] = React.useState(new Date(hour)); 
+    const [localHour, setLocalHour] = React.useState(new Date(hour));   
+    const todos = useSelector(state => state.todos.todos); 
+    const dispatch = useDispatch();
+     
+    const handleDeleteTodo = async () => { 
+        dispatch(deleteTodoReducer(id)); 
+        try { 
+            await AsyncStorage.setItem('@Todos', JSON.stringify( 
+                todos.filter(todo => todo.id !== id)
+            )); 
+            console.log('Todo deleted successfully');
+        } catch (e) { 
+            console.log(e);
+        }
+
+    }
 
     return (  
         
-        <View style={styles.container}>   
-            <Checkbox 
-                id={id} 
-                text={text} 
-                isCompleted={isCompleted} 
-                isToday={isToday} 
-                hour={hour} 
-            /> 
-            <View>  
-            <Text style={ isCompleted ? [styles.text, {textDecorationLine: 'line-through', color: '#73737330'}] : styles.text}>{text}</Text> 
-            <Text style={ isCompleted ? [styles.time, {textDecorationLine: 'line-through', color: '#73737330'}] : styles.time}>{moment(localHour).format('LT')}</Text>
-            </View>
+        <View style={styles.container}>  
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}> 
+                <Checkbox 
+                    id={id} 
+                    text={text} 
+                    isCompleted={isCompleted} 
+                    isToday={isToday} 
+                    hour={hour} 
+                /> 
+                <View>  
+                <Text style={ isCompleted ? [styles.text, {textDecorationLine: 'line-through', color: '#73737330'}] : styles.text}>{text}</Text> 
+                <Text style={ isCompleted ? [styles.time, {textDecorationLine: 'line-through', color: '#73737330'}] : styles.time}>{moment(localHour).format('LT')}</Text>
+                </View>  
+                 
+            </View>  
+             
+             
+            <TouchableOpacity onPress={handleDeleteTodo}> 
+                <MaterialIcons name="delete-outline" size={24} color="#73737330"/>
+            </TouchableOpacity>
+
         
         </View>
     )
@@ -38,7 +67,8 @@ const styles = StyleSheet.create({
         marginTop: 30,
         marginBottom: 5, 
         flexDirection: 'row', 
-        alignItems: 'center'
+        alignItems: 'center', 
+        justifyContent: 'space-between',
     }, 
     text: {
         fontSize: 15, 
