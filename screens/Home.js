5c79fad9cceb8d03b42e2e76fb41e41589c60e22
LizandroBackEnd <codeversebacked@gmail.com>
2024-04-03 15:24:56 -0600

    ci: Add function the home in the task | Dev: Lizandro

--- screens/Home.js
+++ screens/Home.js
@@ -96,30 +96,72 @@ export default function Home() {
      
     
 
-  return ( 
-    <View style={styles.container}>  
-        <Image  
-            source={{ uri: 'https://i.pinimg.com/564x/a9/b2/fd/a9b2fdb12dcf8a29b82b1ba291bcefac.jpg' }} 
-            style={styles.pic} 
-        />    
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}> 
-            <Text style={styles.title}>Today</Text>  
-            <TouchableOpacity onPress={handleHidePress}>  
-            <Text style={{ color: '#3478f6' }}>{isHiden ? "Show Completed" : "Hide Completed"}</Text>     
-            </TouchableOpacity> 
-        </View> 
+    return (
+      todos.length > 0 ? (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: 'https://i.pinimg.com/564x/a9/b2/fd/a9b2fdb12dcf8a29b82b1ba291bcefac.jpg' }}
+            style={styles.pic}
+          />
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Text style={styles.title}>Today</Text>
+            <TouchableOpacity onPress={handleHidePress}>
+              <Text style={{ color: '#3478f6' }}>{isHiden ? "Show Completed" : "Hide Completed"}</Text>
+            </TouchableOpacity>
+          </View>
+          {todos.filter(todo => moment(new Date(todo.hour)).isSame(moment(), 'day')).length > 0 ? (
+            <TodoList todosData={todos.filter(todo => moment(new Date(todo.hour)).isSame(moment(), 'day'))} />
+          ) : ( 
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
+              <Image
+                source={require('../assets/nothingToday.png')}
 
-        <TodoList todosData={todos.filter(todo => moment(new Date(todo.hour)).isSame(moment(), 'day'))} /> 
+                style={{ width: 150, height: 150, marginBottom: 20, resizeMode: 'contain' }}
+              />
+              <Text style={{ fontSize: 13, color: '#000', fontWeight: 'bold' }}>CONGRATS!</Text>
+              <Text style={{ fontSize: 13, color: '#737373', fontWeight: '500' }}>You don't have any task for today, enjoy your day.</Text>
+            </View>
+            // Aquí puedes poner el código para mostrar cuando no hay tareas para hoy
+          )}
+          <Text style={styles.title}>Tomorrow</Text>
+          {todos.filter(todo => moment(new Date(todo.hour)).isAfter(moment(), 'day')).length > 0 ? (
+            <TodoList todosData={todos.filter(todo => moment(new Date(todo.hour)).isAfter(moment(), 'day'))} />
+          ) : ( 
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
+              <Image
+                source={require('../assets/nothingTomorrow.png')}
 
-        <Text style={styles.title}>Tomorrow</Text>  
-        <TodoList todosData={todos.filter(todo => moment(new Date(todo.hour)).isAfter(moment(),'day'))} />
-        <TodoList /> 
-         
-        <TouchableOpacity onPress={() => navigation.navigate("Add")} style={styles.button}>   
+                style={{ width: 150, height: 150, marginBottom: 20, resizeMode: 'contain' }}
+              />
+              <Text style={{ fontSize: 13, color: '#000', fontWeight: 'bold' }}>CONGRATS!</Text>
+              <Text style={{ fontSize: 13, color: '#737373', fontWeight: '500' }}>You don't have any task for today, enjoy your day.</Text>
+            </View>
+            // Aquí puedes poner el código para mostrar cuando no hay tareas para mañana
+          )}
+          <TouchableOpacity onPress={() => navigation.navigate("Add")} style={styles.button}>
             <Text style={styles.plus}>+</Text>
-        </TouchableOpacity>
-    </View>
-  );
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: 'https://i.pinimg.com/564x/a9/b2/fd/a9b2fdb12dcf8a29b82b1ba291bcefac.jpg' }}
+            style={styles.pic}
+          />
+          <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}> 
+          <Image
+                source={require('../assets/nothing.png')}
+                style={{ width: 150, height: 150, marginBottom: 20, resizeMode: 'contain' }}
+              />
+            <Text style={{ fontSize: 13, color: '#000', fontWeight: 'bold' }}>NICE!</Text>
+            <Text style={{ fontSize: 13, color: '#737373', fontWeight: '500' }}>Nothing is scheduled.</Text>
+          </View> 
+          <TouchableOpacity onPress={() => navigation.navigate("Add")} style={styles.button}>
+            <Text style={styles.plus}>+</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    );
 }
 
 const styles = StyleSheet.create({
